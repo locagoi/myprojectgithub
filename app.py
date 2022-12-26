b7298e4bc0da1ddad885a27b1e280e2b7b641160
lucac <lucaceglie123@gmail.com>
2022-12-26 19:59:52 +0100

    commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("/index.html")
+
+@app.route("/projects.html")
+def projects():
+    return render_template("/projects.html")
+
+@app.route("/team.html")
+def team():
+    return render_template("/team.html")
+
+@app.route("/services.html")
+def services():
+    return render_template("/services.html")
+
+@app.route("/contacts.html")
+def contacts():
+    return render_template("/contacts.html")
+
+@app.route("/index.html")
+def index():
+    return redirect(url_for("home"))
+
